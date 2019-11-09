3ed680757849df1387e41102a276f028f6469c87
Lucas Gouvea <lucasgouvea93@gmail.com>
2019-11-09 19:35:50 -0300

    readme revision, env.js host param

--- client/env.js
+++ client/env.js
@@ -1,7 +1,7 @@
 const envFactory = (window) => {
   window.env = window.env || {};
-  window.env.HOST = 'http://35.203.39.237';
-  window.env.PORT = 80;
+  window.env.HOST = '35.203.39.237';
+  window.env.PORT = 8080;
 };
 
 envFactory(this);
