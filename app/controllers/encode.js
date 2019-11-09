2f267c03995d2a174f0b5f2ba82cd445256d579d
Lucas Gouvea <lucasgouvea93@gmail.com>
2019-11-09 19:58:47 -0300

    additional business logic, more tests

--- app/controllers/encode.js
+++ app/controllers/encode.js
@@ -1,12 +1,18 @@
 const { encode } = require('../services/encode');
 const { create } = require('../mongo/repository/encodeLog');
 
+const errLength = 'Text Message > 255 chars';
+
 exports.encode = (req, res) => {
   const { textMessage } = req.body;
   const numberSequence = encode(textMessage);
-  const encodeLog = { numberSequence, textMessage };
-  create(encodeLog).then(
-    () => res.status(200).send({ numberSequence }),
-    (err) => res.status(500).send({ err }),
-  );
+  if (numberSequence === null) {
+    res.status(400).send({ errLength });
+  } else {
+    const encodeLog = { numberSequence, textMessage };
+    create(encodeLog).then(
+      () => res.status(200).send({ numberSequence }),
+      (err) => res.status(500).send({ err }),
+    );
+  }
 };
--- test/api/encode.js
+++ test/api/encode.js
@@ -4,9 +4,15 @@ const { endpoint } = require('../config');
 
 const { expect } = chai;
 
+const textMessage = 'Lorem Ipsum is simply dummy text of the printing and '
+  + 'typesetting industry. Lorem Ipsum has been the industry'
+  + 's standard dummy text ever since the 1500s, when an unknown'
+  + ' printer took a galley of type and scrambled it to make a type'
+  + ' specimen book. It has survived not only five centuries, but also';
+
 const options = {
   method: 'post',
-  body: { textMessage: 'TESTE DE MESA' },
+  body: { textMessage },
   json: true,
   url: `${endpoint.url}/encode`,
 };
@@ -14,9 +20,9 @@ const options = {
 describe('Encoder API', () => {
   it('Should handle a request to encode a text message and send the encoded'
   + ' number sequence back', (done) => {
-    request(options, (err, _, body) => {
+    request(options, (err, res) => {
       expect(err).to.be.equal(null);
-      expect(body.numberSequence).to.be.equals('833777783303_33063377772');
+      expect(res.statusCode).to.be.equal(400);
       done();
     });
   });
